b57e7ebd190d24621a2bc38c596e0da5ab892095
Abubakr0007 <107811306+Abubakr0007@users.noreply.github.com>
2022-10-07 18:34:33 +0500

    date

---
+++ index.js
@@ -0,0 +1,94 @@
+// let data1 = new Date(1000000000000)
+// let data2 = new Date('2022-10-06 18:47:54:15')
+// let data3 = new Date(2022,08,15,15,25,45)
+// console.log(data1)
+// console.log(data2.getDate())
+// data2.setDate(11)
+// console.log(data2.getDate())
+// console.log(data3)
+// data3.setDate(17)
+// console.log(data3)
+
+// let person = {};
+// person.name = prompt('Enter your name: ')
+// person.email = prompt('Enter your email: ')
+// person.password = prompt('Enter your password: ')
+// person.phone = prompt('Enter your phone: ')
+// person.dateOfBirth = prompt('Enter your date of birth: ').split('.').reverse().join('-')
+// person.getAge = function() {
+//     let date = new Date(this.dateOfBirth);
+//     let now = new Date();
+//     return `
+//     ${((now.getFullYear() - date.getFullYear())*365)} 
+//     ${((now.getFullYear() - date.getFullYear())*12)} 
+//     ${Math.round(((now.getFullYear() - date.getFullYear())*365)/7)}
+//     ${(((now.getFullYear() - date.getFullYear())*365))*24} 
+//     ${((((now.getFullYear() - date.getFullYear())*365))*24)*60}
+//     ${(((((now.getFullYear() - date.getFullYear())*365))*24)*60)*60} 
+//     `;
+// }
+// console.log(person.getAge())
+// console.log(per
+
+
+
+
+                                                                   //homework
+
+                                                                    //String date
+// let data1 = new Date()
+// console.log(data1)
+
+                                                                    // date 
+// let data2 = new Date('2022-10-06 18:47:54:15')
+// console.log(date2)
+
+                                                                   //Date one by one
+// let data3 = new Date(2022,08,15,15,25,45)
+// console.log(data3)
+
+                                                                    //Methods Get
+// let data1 = new Date()
+
+// console.log(data1.getFullYear())
+
+// console.log(data1.getMonth())
+
+// console.log(data1.getDate())
+
+// console.log(data1.getDay())
+
+// console.log(data1.getHours())
+
+// console.log(data1.getMinutes())
+
+// console.log(data1.getSeconds())
+
+// console.log(data1.getMilliseconds())
+
+                                                                     //Methods Set
+
+// let data2 = new Date()
+
+// data2.setFullYear(2000)
+// console.log(data2)
+
+// data2.setMonth(11)
+// console.log(data2)
+
+// data2.setDate(10)
+// console.log(data2)
+
+// data2.setHours(5)
+// console.log(data2)
+
+// data2.setMinutes(14)
+// console.log(data2)
+
+// data2.setSeconds(59)
+// console.log(data2)
+
+// data2.setMilliseconds(1000)
+// console.log(data2)
+
+
